64a74fabfabc922ee13dc1309906b5e659751cb1
Marat Saldakov <wezeltonick@gmail.com>
2017-09-18 14:42:31 +0300

    hello

--- src/systemd-monitoring/service/service.go
+++ src/systemd-monitoring/service/service.go
@@ -23,6 +23,7 @@ type Service struct {
     changed    bool
     isActive   bool
     isEnabled  bool
+    timeout_sec time.Duration
     //
 }
 
@@ -47,13 +48,15 @@ func(s *Service)String() string {
 func CheckSystemdService(service_name string)(*Service,error){
     //
     var service Service
+    service.name        = service_name
+    service.timeout_sec = 5
     //
-    out_active_byte,_ := exec.Command(systemctl_path,"is-active",service_name).Output()
-    out_enabled_byte,_:= exec.Command(systemctl_path,"is-enabled",service_name).Output()
-    out_active  := string(out_active_byte)
-    out_enabled := string(out_enabled_byte)
-    out_active  = strings.Replace(out_active, "\n", "", 1)
-    out_enabled = strings.Replace(out_enabled, "\n","", 1)
+    out_active_byte,_  := exec.Command(systemctl_path,"is-active",service_name).Output()
+    out_enabled_byte,_ := exec.Command(systemctl_path,"is-enabled",service_name).Output()
+    out_active         := string(out_active_byte)
+    out_enabled        := string(out_enabled_byte)
+    out_active         =  strings.Replace(out_active, "\n", "", 1)
+    out_enabled        =  strings.Replace(out_enabled, "\n","", 1)
     //
     //if err_active != nil || err_enabled != nil {
     //    fmt.Printf("checkSystemdService:err_active:%v\terr_enabled:%v\t\n",err_active,err_enabled)
@@ -72,6 +75,9 @@ func CheckSystemdService(service_name string)(*Service,error){
         service.isEnabled = false
     }
     pid,err := GetMainPid(service_name)
+    if pid == dead_pid {
+        service.timeout_sec = 30
+    }
     if err == nil {
         service.pid = pid
     } else {
@@ -125,6 +131,7 @@ func NewServiceChain(service_names []string, updates chan string)(c *Chain,err e
     //
     //
     var chain Chain
+    chain.services = make([]*Service,0)
     for i:= range service_names {
         service_name := service_names[i]
         s,err        := CheckSystemdService(service_name)
@@ -136,15 +143,15 @@ func NewServiceChain(service_names []string, updates chan string)(c *Chain,err e
     if len(chain.services) == 0 { return nil, serviceChainIsEmpty }
     hostname,err := os.Hostname()
     if err == nil {
-        c.hostname = hostname
-        return &chain, nil
+        chain.hostname = hostname
+        //return &chain, nil
     } else {
         return nil, err
     }
-    chain.timeout_sec    = 5
-    chain.updates    = make(chan string)
-    chain.processing = make(chan *Service)
-    return &chain,nil
+    chain.timeout_sec  = 5
+    chain.updates      = updates
+    chain.processing   = make(chan *Service)
+    return &chain, nil
     //
     //
 }
@@ -173,23 +180,50 @@ func (c *Chain)Proceed()(){
 
 func (c *Chain)Proceed2()(){
     //
+    // init service chain
     //
+    // let's create chan with size equals to len of service to prevent deadlock
     if c == nil { return }
+    //
+    go func() {
+        for i := range c.services {
+        s := c.services[i]
+        if s != nil {
+           name                    := s.name
+           //fmt.Printf("\nProceed : --%v--\n", s.name)
+           currentServiceState,err := CheckSystemdService(name)
+           if currentServiceState != nil && err == nil {
+               c.processing <- currentServiceState
+           }
+        }
+        }
+    }()
+    //
+    //
+    //
     for  {
         select {
             case s := <-c.processing:
+                //fmt.Printf("\n>>> Processing service :%v\n",s)
+                go func() {
                 if s != nil {
                     name                    := s.name
                     currentServiceState,err := CheckSystemdService(name)
+                    //fmt.Printf("\n>>> Processing:current_service_state:%v\terr:%v\n",currentServiceState,err)
                     if currentServiceState != nil && err == nil {
                         changes,err := Compare(s, currentServiceState)
                         if err == nil && len(changes)>0 {
                             for i:= range changes {
                                 c.updates <- "hostname:"+c.hostname+":\n"+changes[i]+"\n"
                             }
+                            // sleep if service has been changed
+                            time.Sleep(time.Second * c.timeout_sec)
                         }
+                        time.Sleep(time.Second * s.timeout_sec)
+                        c.processing <- currentServiceState
                     }
                 }
+                }()
             default:
                 time.Sleep(time.Second * c.timeout_sec)
         }
--- src/systemd-monitoring/test_service.go
+++ src/systemd-monitoring/test_service.go
@@ -1,9 +1,25 @@
 package main
 
 import "fmt"
+import "time"
 import "systemd-monitoring/service"
 
 func main() {
-    s,err:=service.CheckSystemdService("cron")
-    fmt.Printf("service: %v err %v\n",s,err)
+    // s,err:=service.CheckSystemdService("cron")
+    // fmt.Printf("service: %v err %v\n",s,err)
+    var services =  []string {"cron","pcap-log"}
+    updates:=make(chan string)
+    chain,err:=service.NewServiceChain(services, updates)
+    go chain.Proceed2()
+    if err == nil {
+        for {
+            select {
+                case u:=<-updates:
+                    fmt.Printf("Update:%v\n",u)
+                default:
+                    time.Sleep(time.Second * 2)
+            }
+
+        }
+    }
 }
